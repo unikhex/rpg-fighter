9daff0331f7e08f9099ffd825c90b6931377e4d5
unikhex <potatoc377@gmail.com>
2022-02-15 18:52:08 +0100

    added rounds

--- source/resources.py
+++ source/resources.py
@@ -7,19 +7,43 @@ class Character:
     
     def __str__(self) -> str:
         return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
-       
-class Goblin:
 
-    def __init__(self , health, attack, armor):
+    def damage(self):
+        return self.attack
+
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        if relative_damage > 0:
+            self.health -= relative_damage
+        if self.health < 0: self.health = 0   
     
+    def get_health(self):
+        return self.health
+    
+    
+
+class Goblin:
+    
+    def __init__(self, health, attack, armor):
         self.health = health
         self.attack = attack
         self.armor = armor
-    
+        
     def __str__(self) -> str:
-        return f"goblin\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
-       
+        return f"Goblin\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
 
+    def damage(self):
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        if relative_damage > 0:
+            self.health -= relative_damage
+        if self.health < 0: self.health = 0
+
+    def get_health(self):
+        return self.health
+    
 
     
 
