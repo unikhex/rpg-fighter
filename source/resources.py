391f6eaf8f5a27cfdce0340bc67b6af3f3d7c4bf
unikhex <potatoc377@gmail.com>
2022-02-15 14:24:03 +0100

    added an enemy class

--- source/resources.py
+++ source/resources.py
@@ -6,8 +6,20 @@ class Character:
         self.armor = armor
     
     def __str__(self) -> str:
-        return-string = f"Name: {self.name}\nHealth: "
-      
+        return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+       
+class Goblin:
+
+    def __init__(self, name, health, attack, armor):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.armor = armor
+    
+    def __str__(self) -> str:
+        return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+       
+
 
     
 
