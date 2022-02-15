cf05ea326f5b9cfbbe549f40df50f104945fbd19
unikhex <potatoc377@gmail.com>
2022-02-15 14:11:45 +0100

    added character class

---
+++ source/resources.py
@@ -0,0 +1,14 @@
+class Character:
+    def __init__(self, name, health, attack, armor):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.armor = armor
+    
+    def __str__(self) -> str:
+        return-string = f"Name: {self.name}\nHealth: "
+      
+
+    
+
+ 
