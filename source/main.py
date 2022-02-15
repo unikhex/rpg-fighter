d7dd1f0cc63b9992b92f63a5251146baa3490a00
unikhex <potatoc377@gmail.com>
2022-02-15 14:32:08 +0100

    did something on the main

--- source/main.py
+++ source/main.py
@@ -1,4 +1,10 @@
 import resources
+from resources import Character, Goblin
 
-if __name__ =="__main__":
-    pass
+if __name__ == "__main__":
+    nemy = Character("Nemy",20,5,3)
+    goblin_one = Goblin(10,3,1)
+
+    print(nemy)
+    print()
+    print(goblin_one)
