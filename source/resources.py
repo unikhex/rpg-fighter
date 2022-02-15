6d2cca8009474597ec193eeef21bcaeb9d8b0b3f
unikhex <potatoc377@gmail.com>
2022-02-15 14:37:07 +0100

    fixed the problem with goblin

--- source/resources.py
+++ source/resources.py
@@ -10,14 +10,14 @@ class Character:
        
 class Goblin:
 
-    def __init__(self, name, health, attack, armor):
-        self.name = name
+    def __init__(self , health, attack, armor):
+    
         self.health = health
         self.attack = attack
         self.armor = armor
     
     def __str__(self) -> str:
-        return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+        return f"goblin\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
        
 
 
