2802ba9307a0a90236c7aec0a7cdc93809ac744c
unikhex <potatoc377@gmail.com>
2022-02-22 15:01:50 +0100

    added choice

--- source/resources.py
+++ source/resources.py
@@ -1,29 +1,31 @@
 class Character:
+    
     def __init__(self, name, health, attack, armor):
         self.name = name
         self.health = health
         self.attack = attack
         self.armor = armor
-    
+        
     def __str__(self) -> str:
         return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
-
-    def damage(self):
-        return self.attack
-
+    
     def take_damage(self, damage):
         relative_damage = damage - self.armor
         if relative_damage > 0:
             self.health -= relative_damage
-        if self.health < 0: self.health = 0   
+        if self.health < 0: self.health = 0
     
+    def get_attack(self): # tidigare damage
+        return self.attack
+
     def get_health(self):
         return self.health
-    def get_attack(self): # was damage before
-        return self.attack
+    
     def get_name(self):
         return self.name
     
+    def get_attributes(self):
+        return self.name, self.health, self.attack, self.armor
 
 class Goblin:
     
@@ -31,12 +33,10 @@ class Goblin:
         self.health = health
         self.attack = attack
         self.armor = armor
-        self.id = id 
+        self.id = id
         
     def __str__(self) -> str:
-        return f"Goblin#{self.id}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
-
-   
+        return f"Goblin #{self.id}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
     
     def take_damage(self, damage):
         relative_damage = damage - self.armor
@@ -52,3 +52,29 @@ class Goblin:
     
     def get_name(self):
         return f"Goblin #{self.id}"
+    
+
+def save_character(chars : list()):
+    save_list = []
+    for character in chars:
+        name, health, attack, armor = character.get_attributes()
+        save_string = f"{name}/{health}/{attack}/{armor}\n"
+        save_list.append(save_string)
+        
+    with open("saved_characters.txt", "w", encoding="utf8") as f:
+        for line in save_list:
+            f.write(line)
+        print("Characters has been saved!")
+
+def load_characters():
+    characters = []
+    with open("saved_characters.txt", "r", encoding="utf8") as f:
+        for line in f.readlines():
+            attributes = line.split("/")
+            char = Character(attributes[0],
+                             int(attributes[1]),
+                             int(attributes[2]),
+                             int(attributes[3]))
+            
+            characters.append(char)
+    return characters    
