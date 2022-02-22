df92d957934926e67c8b0cddc7e40f3587d9ef9b
unikhex <potatoc377@gmail.com>
2022-02-22 15:01:30 +0100

    added save characters and loading those characters.

--- source/main.py
+++ source/main.py
@@ -1,43 +1,45 @@
-from errno import ENETDOWN
-from operator import le
-from os import remove
-from winsound import PlaySound
 import resources
-from resources import Character, Goblin
+from resources import Character, Goblin, save_character, load_characters
 from random import randint, shuffle, choice
+
 def fight(players : list, enemies : list):
-    participants = players + enemies # create an order of who begins   
+    participants = players + enemies # skapa initiativordning 
     shuffle(participants)
+    
     for char in participants:
         target = ""
-        # is character a player or goblin
+        # är karaktären en spelare eller en goblin?
         if char in players:
             target = choice(enemies)
         else:
             target = choice(players)
+            
         target.take_damage(char.get_attack())
+        
         if target.get_health() == 0:
-            print(f"{target.get_name()} has died") 
+            print(f"{target.get_name()} has died!")
             if type(target) == Goblin:
                 enemies.remove(target)
             else:
                 players.remove(target)
             participants.remove(target)
         else:
-            print(f"{target.get_name()} has {target.get_health()} hp remaining")
-
+            print(f"{target.get_name()} has {target.get_health()} hp remaining.")
+        
         if len(enemies) == 0 or len(players) == 0:
-            break    
-
-
+            break
+            
+    
 if __name__ == "__main__":
     enemies = []
-    players = []   
-    nemy = Character("Nemy", 20, 5, 2)
-    nick = Character("Nick", 15, 2, 1)
-    players.append(nemy)
-    players.append(nick)
-
+    players = load_characters()
+    #players = []
+    
+    # emy = Character("Emy", 20, 5, 2)
+    # nick = Character("Nick", 15, 2, 1)
+    # players.append(emy)
+    # players.append(nick)
+    
     enemies.append(Goblin(10, 3, 2, 1))
     enemies.append(Goblin(15, 2, 1, 2))
     enemies.append(Goblin(12, 3, 1, 3))
@@ -50,8 +52,13 @@ if __name__ == "__main__":
         round +=1
     
     if len(enemies) == 0:
-        print("The players won")
-    elif len(players) == 0:
-        print("The Goblins won")
-
+        print("The players won!")
+        print("Would you like to save the remaining characters?")
+        save_progress = input("(y/n): ")
+        if save_progress == "y":
+            save_character(players)
+        else:
+            print("No progress was saved.")
         
+    elif len(players) == 0:
+        print("The Goblins won!")
